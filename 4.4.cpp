8b4340c0bfcf4821d7afb1d2a185f7712dea02c9
Anurag Pratap Singh <118565462+Anurag22102@users.noreply.github.com>
2024-05-01 00:07:41 +0530

    Update 4.4.cpp

--- 4.4.cpp
+++ 4.4.cpp
@@ -1,86 +1,117 @@
 #include <iostream>
-using namespace std; 
+#include <vector>
+#include <algorithm>
+using namespace std;
 
-class DSU { 
-  int* parent; 
-  int* rank; 
 
-public: 
-  DSU(int n) { 
-    parent = new int[n]; 
-    rank = new int[n]; 
+struct Edge {
+    int src, dest, weight;
+};
 
-    for (int i = 0; i < n; i++) { 
-      parent[i] = -1; 
-      rank[i] = 1; 
-    } 
-  } 
 
-  int find(int i) { 
-    if (parent[i] == -1) 
-      return i; 
+struct Subset {
+    int parent;
+    int rank;
+};
 
-    return parent[i] = find(parent[i]); 
-  } 
 
-  void unite(int x, int y) { 
-    int s1 = find(x); 
-    int s2 = find(y); 
+class Graph {
+    int V, E;
+    vector<Edge> edges;
 
-    if (s1 != s2) { 
-      if (rank[s1] < rank[s2]) { 
-        parent[s1] = s2; 
-      } 
-      else if (rank[s1] > rank[s2]) { 
-        parent[s2] = s1; 
-      } 
-      else { 
-        parent[s2] = s1; 
-        rank[s1] += 1; 
-      } 
-    } 
-  } 
-}; 
 
-class Graph { 
-  vector<vector<int> > edgelist; 
-  int V; 
+public:
+    Graph(int V, int E) {
+        this->V = V;
+        this->E = E;
+    }
+    
+    void addEdge(int src, int dest, int weight) {
+        Edge edge;
+        edge.src = src;
+        edge.dest = dest;
+        edge.weight = weight;
+        edges.push_back(edge);
+    }
+    
+    int find(Subset subsets[], int i) {
+        if (subsets[i].parent != i)
+            subsets[i].parent = find(subsets, subsets[i].parent);
+        return subsets[i].parent;
+    }
+    
+    void Union(Subset subsets[], int x, int y) {
+        int xroot = find(subsets, x);
+        int yroot = find(subsets, y);
 
-public: 
-  Graph(int V) { this->V = V; } 
 
-  void addEdge(int x, int y, int w) { 
-    edgelist.push_back({ w, x, y }); 
-  } 
+        if (subsets[xroot].rank < subsets[yroot].rank)
+            subsets[xroot].parent = yroot;
+        else if (subsets[xroot].rank > subsets[yroot].rank)
+            subsets[yroot].parent = xroot;
+        else {
+            subsets[yroot].parent = xroot;
+            subsets[xroot].rank++;
+        }
+    }
+    
+    void KruskalMST() {
+        vector<Edge> result;
+        
+        sort(edges.begin(), edges.end(), [](const Edge &a, const Edge &b) {
+            return a.weight < b.weight;
+        });
+        
+        Subset *subsets = new Subset[V];
+        
+        for (int v = 0; v < V; ++v) {
+            subsets[v].parent = v;
+            subsets[v].rank = 0;
+        }
+        
+        int i = 0, e = 0;
+        while (e < V - 1 && i < E) {
+            
+            Edge next_edge = edges[i++];
 
-  void kruskals_mst() { 
-    sort(edgelist.begin(), edgelist.end()); 
 
-    DSU s(V); 
-    int ans = 0; 
-    for (auto edge : edgelist) { 
-      int w = edge[0]; 
-      int x = edge[1]; 
-      int y = edge[2]; 
+            int x = find(subsets, next_edge.src);
+            int y = find(subsets, next_edge.dest);
+            
+            if (x != y) {
+                result.push_back(next_edge);
+                Union(subsets, x, y);
+                e++;
+            }
+        }
+        
+        cout << "Edges of MST:\n";
+        int minimumCost = 0;
+        for (i = 0; i < e; ++i) {
+            cout << result[i].src << " - " << result[i].dest << "  Weight: " << result[i].weight << endl;
+            minimumCost += result[i].weight;
+        }
+        cout << "Minimum Cost of MST: " << minimumCost << endl;
 
-      if (s.find(x) != s.find(y)) { 
-        s.unite(x, y); 
-        ans += w; 
-      } 
-    } 
-    cout << "Minimum Cost Spanning Tree: " << ans; 
-  } 
-}; 
 
-int main() { 
-  Graph g(4); 
-  g.addEdge(0, 1, 10); 
-  g.addEdge(1, 3, 15); 
-  g.addEdge(2, 3, 4); 
-  g.addEdge(2, 0, 6); 
-  g.addEdge(0, 3, 5); 
+        delete[] subsets;
+    }
+};
 
-  g.kruskals_mst(); 
 
-  return 0; 
+int main() {
+    int V = 4;
+    int E = 5;
+    Graph graph(V, E);
+    
+    graph.addEdge(0, 1, 10);
+    graph.addEdge(0, 2, 6);
+    graph.addEdge(0, 3, 5);
+    graph.addEdge(1, 3, 15);
+    graph.addEdge(2, 3, 4);
+    
+    graph.KruskalMST();
+
+
+    return 0;
 }
